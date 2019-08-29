5f0534c150ea22edd6c3e0ab478712eed3617bdb
Gulam Qasim <GQasim@spartaglobal.com>
2019-08-29 23:22:12 +0100

    Third commit

--- Labs Homework/Labs Homework/Program.cs
+++ Labs Homework/Labs Homework/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Web.Script.Serialization; //Adds this libary from the references section
 
 namespace Labs_Homework
 {
@@ -15,9 +12,18 @@ namespace Labs_Homework
             int multipledNumbers = taskOne.Product(5, 10, 15, out addedNumbers); //Output is 750
             Console.WriteLine("All the added numbers: " + addedNumbers);
             Console.WriteLine("all the numbers multiplied: " + multipledNumbers);
-        }
 
-      
+
+            Cat TibbyTheCat = new Cat(2, "Tibby"); //instance of cat with name and age
+            Cat.AgeOfCat(TibbyTheCat);
+            Cat.AgeOfCat(TibbyTheCat);
+
+            var json = new JavaScriptSerializer().Serialize(TibbyTheCat);
+            Console.WriteLine(json.ToString());
+
+
+            Console.Read();
+        }
     }
 
     class labTaskOne
@@ -31,15 +37,44 @@ namespace Labs_Homework
 
     class Cat
     {
+        /* Explains this function down here
+         * This is a constructor, when I created it
+         * it had to be the same name as the class.
+         * 
+         * So when I instantiate a class/object it forces me to give
+         * it a input
+         * 
+         * In this case when I make a new cat you always have to give it
+         * a name and age
+         */
+
         private string name;
         private int age;
-        public int Cat(int age, string name)
+
+        public Cat(int age, string name)
         {
             this.name = name;
             this.age = age;
-
         }
 
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public int Age
+        {
+            get => age;
+            set => age = value;
+        }
+
+        public static object AgeOfCat(Cat cat)
+        {
+            cat.age = cat.age + 5;
+
+            return cat;
+        }
 
     }
 }
